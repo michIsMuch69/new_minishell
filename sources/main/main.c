ac1facedea9f7544be1fc2b938f1af6cafcbacb2
Jean-Michel Dusserre <jedusser@z3r10p1.42lyon.fr>
2024-05-22 17:24:29 +0200

    teste fork

--- sources/main/main.c
+++ sources/main/main.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <readline/readline.h> 
 #include <readline/history.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 // ###### INCLUDES ######
 
@@ -24,18 +26,29 @@ int main (int argc, char **argv, char **envp)
 	char 	*prompt;
 	char	*env;
 	t_cmd	*lst;
-
+	pid_t 	pid;
+	int 	i;
 	if (argc != 1)
-		return (ft_perror("arguments are invalid\n"));	
-	while (1)
+		return (ft_perror("arguments are invalid\n"));
+	printf("coucou\n");
+	pid = fork();
+	if(pid == 0)
+		execve("/bin/ls", argv, envp);
+	else
+		waitpid(pid, &i, 0);
+	if(pid != 0)
 	{
-		env = get_pwd();
-		prompt = readline(env);
-		add_history(prompt);
-		printf("%s\n", prompt);
-		lst = parse_prompt(prompt);
-		//exec(lst);
-		free(env);
+		while (1)
+		{
+			env = get_pwd();
+			prompt = readline(env);
+			add_history(prompt);
+			printf("%s\n", prompt);
+			//lst = parse_prompt(prompt);
+			//exec(lst);
+			free(env);
+		}
+
 	}
 	return (0);
 }
