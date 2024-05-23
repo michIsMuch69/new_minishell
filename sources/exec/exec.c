63f764d9c6c0a4ba25b751444877b1c7c1746dde
Jean-Michel Dusserre <jedusser@z3r10p3.42lyon.fr>
2024-05-23 16:31:55 +0200

    path finding for execs

--- sources/exec/exec.c
+++ sources/exec/exec.c
@@ -6,7 +6,62 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:42:33 by jedusser          #+#    #+#             */
-/*   Updated: 2024/05/22 16:42:34 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:23:25 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return(s1[i] - s2[i]);
+}
+
+int	exec_found(const char *dirname, char *exec_searched)
+{
+	DIR				*dir;
+	struct dirent	*entity;
+	
+	dir = opendir(dirname);
+	if(!dir)
+		return (-1);
+	entity = readdir(dir);
+	while (entity != NULL)
+	{
+		if(ft_strcmp(entity->d_name, exec_searched) == 0)
+			return(1);
+		entity = readdir(dir);
+	}
+	closedir(dir);
+	return (0);
+}
+
+//function our change de dir.
+char	*check_all_dirs(char **envp, char *exec_searched)
+{
+	const char	*paths = getenv("PATH");
+	char		**path_list;
+	path_list = ft_split(paths, ':');
+	if(!path_list)
+		return (NULL);
+	int i = 0;
+	while (path_list[i])
+	{
+		if(exec_found(path_list[i], exec_searched) == 1)
+			return(path_list[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+
+// check absolutely if file is an executable (entity->d_type)//
+// an if rights are OK
--- sources/main/main.c
+++ sources/main/main.c
@@ -20,36 +20,38 @@ t_cmd	*parse_prompt(char *prompt);
 // ###### PROTO ######
 
 
+void echo(char *arguments);
+int	exec_found(const char *dirname, char *exec_searched);
+char	*check_all_dirs(char **envp, char *exec_searched);
 
 int main (int argc, char **argv, char **envp)
 {
+
+	char	*arguments = "ceci peut etre $PATH ecrit apres echo";
+	char	*exec_searched = "ls";
 	char 	*prompt;
 	char	*env;
 	t_cmd	*lst;
 	pid_t 	pid;
 	int 	i;
+
 	if (argc != 1)
 		return (ft_perror("arguments are invalid\n"));
-	printf("coucou\n");
-	pid = fork();
-	if(pid == 0)
-		execve("/bin/ls", argv, envp);
-	else
-		waitpid(pid, &i, 0);
-	if(pid != 0)
+	while (1)
 	{
-		while (1)
-		{
-			printf("%d\n", getpid());
-			env = get_pwd();
-			prompt = readline(env);
-			add_history(prompt);
-			printf("%s\n", prompt);
-			//lst = parse_prompt(prompt);
-			//exec(lst);
-			free(env);
-		}
-
+		//printf("%d\n", getpid());
+		env = get_pwd();
+		prompt = readline(env);
+		add_history(prompt);
+		//printf("%s\n", prompt);
+		echo(arguments);
+		//printf("%d\n", exec_found("/bin"));
+		printf("%s\n", check_all_dirs(envp, exec_searched));
+		execve(check_all_dirs(envp, exec_searched), argv, envp);
+		//echo(prompt);
+		//lst = parse_prompt(prompt);
+		//exec(lst);
+		free(env);
 	}
 	return (0);
 }
