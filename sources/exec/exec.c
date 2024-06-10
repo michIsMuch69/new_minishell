aa28fd17c4954c67f2660a8aaf775b4a367590c3
¨michmuch¨ <jean-michel.dusserre@hotmail.fr>
2024-06-10 12:13:45 +0200

    change error in handle_child from retunrn -1 to exit

--- sources/exec/exec.c
+++ sources/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 11:07:16 by fberthou          #+#    #+#             */
-/*   Updated: 2024/06/10 10:49:51 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/06/10 12:11:23 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,50 +47,64 @@ char	*skip_redir_symbol(char *token_file, bool direction)
 
 }
 
-int	handle_child(int i, int fds[2], int tab_size, int prev_fd, t_data *data)
+int	redir_input(t_data *data, int i, int prev_fd)
 {
-	
-	int	input_fd; 
-	int	output_fd;
-
+	int		input_fd;
+	char	*input_file;
 	
 	if (data[i].input.size)
 	{
-		char *input_file = skip_redir_symbol(data[i].input.tab[0], 0);
-		
+		input_file = skip_redir_symbol(data[i].input.tab[0], 0);
 		input_fd = open(input_file, O_RDONLY);
-		if(dup2(input_fd, STDIN_FILENO))
-			return (-1);
-		close(input_fd);
+		if(input_fd == -1)
+			return (perror("Failed to open input file"), -1);
+		if (dup2(input_fd, STDIN_FILENO) == -1)
+			return(perror("Failed to redirect standard input"), close(input_fd), -1);
 	}
 	else if (i > 0) //not first cmd.
 	{
 		if(dup2(prev_fd, STDIN_FILENO) == -1)
-			return (-1); // --> proteger appel a pid, apres refacto);
+			return (perror("Failed to duplicate previous fd for  input"), -1); // --> proteger appel a pid, apres refacto);
 		close(prev_fd);
 	}
+	return (0);
+}
+int	redir_output(t_data *data, int i, int tab_size, int *fds)
+{
+	int		output_fd;
+	char	*output_file;
 	if(data[i].output.size)
 	{
-		char *output_file = skip_redir_symbol(data[i].output.tab[0], 1);
+		output_file = skip_redir_symbol(data[i].output.tab[0], 1);
 		output_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if(output_fd == -1)
+			return (perror("Failed to open output file"), -1);
 		if(dup2(output_fd, STDOUT_FILENO) == -1)
-			return (-1);
+			return (perror("Failed to redirect standard output"), -1);
 		close(output_fd);
 	}
 	else if (i < tab_size - 1) // not last cmd.
 	{
 		if(dup2(fds[1], STDOUT_FILENO) == -1)
-			return (-1);
+			return (perror("Failed to duplicate pipe fd for output"), -1);
 		close(fds[1]);
 	}
-	if (exec(i, data, tab_size) == -1)
-		return (-1);
-	close(fds[0]);
 	return (0);
+}
+void	handle_child(int i, int fds[2], int tab_size, int prev_fd, t_data *data)
+{
+	if (redir_input(data, i, prev_fd) == -1)
+		 exit(EXIT_FAILURE);
+	if (redir_output(data, i , tab_size, fds) == -1)
+		 exit(EXIT_FAILURE);
+	if (exec(i, data, tab_size) == -1)
+		exit(EXIT_FAILURE);
+	close(fds[0]);
+	close(fds[1]);
+	exit(EXIT_SUCCESS);
 }
 // test : 
 // -  cat < file2.txt | rev > file1.txt
-
 
 void	handle_parent(int i, int fds[2], int prev_fd, int tab_size)
 {
@@ -114,15 +128,12 @@ int	pipex(int tab_size, t_data *data)
 	while (i < tab_size )
 	{
 		if (pipe(fds) == -1)
-			return (perror("pipe failed"), -1);
+			return (perror("Pipe failed"), -1);
 		pid = fork();
 		if (pid == -1)
-			return (perror("fork failed"), -1);
+			return (perror("Fork failed"), -1);
 		else if (pid == 0)
-		{
-			if (handle_child(i, fds, tab_size, prev_fd, data))
-				return (-1);
-		}
+			handle_child(i, fds, tab_size, prev_fd, data);
 		else
 		{
 			handle_parent(i, fds, prev_fd, tab_size); //retour d'erreur ?
